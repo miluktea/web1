00cea5ce555e2428b8e68a4bd962a26d058afc79
miluktea <82809870+miluktea@users.noreply.github.com>
2022-03-22 20:07:00 +0900

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,20 @@
+#! C:\Program Files\Python310\python.exe
+#number
+print(1)
+#string
+print('Hello world') #Integer
+#Boolean
+print(True)
+print(False)
+#Expression
+print(1+1)
+print('Hello'+'world')
+#Comparison operator
+print(1==1)#True
+print(1<2)#True
+print(2<1)#False
+#Membership operator
+print('world' in 'Hello world')#True
+
+import os.path
+print(os.path.exists('boolean2.py'))
