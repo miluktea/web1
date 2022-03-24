a29206c4ab12597d1740b8ceee8ceddee5d56967
miluktea <82809870+miluktea@users.noreply.github.com>
2022-03-24 20:46:59 +0900

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,26 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_pwd == '111111':
+    print('Hello master')
+    print('hi')
+else:
+    print('Who are you?')
+'''
+
+'''
+if user_id == 'egoing':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'egoing' and if user_pwd == '111111':
+    print('Hello master')
+elif user_id == 'loooow' and if user_pwd == '222111':
+else:
+    print('Who are you?')
