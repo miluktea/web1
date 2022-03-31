e6c3e7e05abb862b6f72ac4cf19c35a086e13d24
miluktea <82809870+miluktea@users.noreply.github.com>
2022-03-31 21:51:13 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+print("Content-Type: text/html")
+print()
 import cgi, os
 
 files = os. listdir('data')
@@ -6,5 +8,22 @@ for item in files:
     listStr = listStr + '<li><a href="index.py?id{name}">{name}</a></li>'.format(name=item)
 
 form = cgi.FieldStorage()
-
+<html>
+<head>
+  <title> WEB1 - Welcome</title>
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="creat.py">create</a>
+  <form action="process_creat.py" method="post">
+    <p><input type="text" name="title" placeholder="title"></p>
+    <p><textarea rows="4" name="description" placeholder="description"></textarea></p>
+    <p><input type="submit"></p>
+    <p></p>
+    </form>
+</body>
+</html>
 '''.format(title=pageId, desc=description, listStr=listStr)
