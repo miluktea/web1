8b1fb5a2b138cba88430e79b1a729c823e4cf01e
miluktea <82809870+miluktea@users.noreply.github.com>
2022-04-01 20:25:46 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -8,6 +8,14 @@ for item in files:
     listStr = listStr + '<li><a href="index.py?id{name}">{name}</a></li>'.format(name=item)
 
 form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+    update_link = '<a href="update.py?id={}">uptate</a>'.format(pageId)
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+print('''<!doctype html>
 <html>
 <head>
   <title> WEB1 - Welcome</title>
@@ -18,12 +26,13 @@ form = cgi.FieldStorage()
     {listStr}
   </ol>
   <a href="creat.py">create</a>
-  <form action="process_creat.py" method="post">
-    <p><input type="text" name="title" placeholder="title"></p>
-    <p><textarea rows="4" name="description" placeholder="description"></textarea></p>
-    <p><input type="submit"></p>
-    <p></p>
-    </form>
+  <form action="process_update.py" method="post">
+       <input type="hidden" name="pageId" value="{form_default_title}">
+       <p><input type="text" name="title" placeholder="title" value="{form_defalt_title}"></p>
+       <p><textarea rows="4" name="description" placeholder="description">{form_default_desciption}</textarea></p>
+       <p><input type="submit"></p>
+       <p></p>
+   </form>
 </body>
 </html>
-'''.format(title=pageId, desc=description, listStr=listStr)
+'''.format(title=pageId, desc=description, listStr=listStr, form_default_title=pageID, form_default_description=description))
--- process_update.py
+++ process_update.py
@@ -1,13 +1,16 @@
 #!/user
 
-import cgi
+import cgi, os
 form = cgi.FieldStorage()
+pageId = form["pageId"].value
 title = form["title"].value
 description = form['description'].value
 
-opened_file = open('data/'+title, 'w')
+opened_file = open('data/'+pageId, 'w')
 opened_file.write(description)
 opened_file.close()
+
+os.rename(pageId, title)
 
 #Redirection
 print("Location: index.py?id="+title)
