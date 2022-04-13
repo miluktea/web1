e58cf835a33b2735cbc160365a2d70a17732a248
miluktea <82809870+miluktea@users.noreply.github.com>
2022-04-13 20:40:40 +0900

    Add files via upload

---
+++ number.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Number & Arithmetic Operator</h1>
+  <h2>1+1</h2>
+  <?php
+  echo 1+1;
+  ?>
+  <?php
+
+  ?>
+  <h2>2*2</h2>
+  <?php
+  echo 2*2;
+  ?>
+</body>
+</html>
