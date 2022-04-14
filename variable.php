c64e5b592644e6f88eb511b3b264333e445adbd9
miluktea <82809870+miluktea@users.noreply.github.com>
2022-04-14 20:54:16 +0900

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+ <body>
+   <h1>Variable</h1>
+   <?php
+   $name = "egoing";
+   echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor" .$name." incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"  .$name." ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ".$name. "occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".$name;
+   ?>
+ </body>
+</html>
