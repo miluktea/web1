0649f41598dda3688aa645f521afa0bff45216dd
miluktea <82809870+miluktea@users.noreply.github.com>
2022-05-04 23:41:41 +0900

    Add files via upload

--- create.php
+++ create.php
@@ -44,14 +44,11 @@
     ?>
    </ol>
    <a href="create.php">create</a>
-   <h2>
-     <?php
-     print_title();
-      ?>
-   </h2>
-     <?php
-      print_description();
-      ?>
+   <form action="create_process.php" method="post">
+     <p><input type="text" name="title" placeholder="title"></p>
+     <p><textarea name="description" placeholder="description"></textarea></p>
+     <p><input type="submit"></p>
+   </form>
 
 </body>
 </html>
