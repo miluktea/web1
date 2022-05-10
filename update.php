338a7fb4d42cea034937ec7e20e702c79b8ad682
miluktea <82809870+miluktea@users.noreply.github.com>
2022-05-10 22:00:13 +0900

    Add files via upload

--- update.php
+++ update.php
@@ -56,6 +56,12 @@
      <?php
       print_description();
       ?>
+      <form action="update_process.php" method="post">
+        <input type="hidden" name="old_title" value="<?=$_GET['id']?>">
+        <p><input type="text" name="title" placeholder="title" value="<?php print_title(); ?>"></p>
+        <p><textarea name="description" placeholder="description"><?php print_description(); ?></textarea></p>
+        <p><input type="submit"></p>
+      </form>
 
 </body>
 </html>
